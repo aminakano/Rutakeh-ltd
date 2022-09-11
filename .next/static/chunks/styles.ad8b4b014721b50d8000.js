fd60dfee00b8d4b5e51cf1199f21788ee1f8938d
Ami Nakano <ami.nakano@outlook.com>
2022-09-11 20:01:14 +0200

    Fixing errors WIP

--- .next/static/chunks/styles.ad8b4b014721b50d8000.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"2mU8":function(n,o,w){},mFPE:function(n,o,w){}}]);
--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-const withImages = require("next-images");
+// const withSass = require("@zeit/next-sass");
+// const withCSS = require("@zeit/next-css");
+// const withImages = require("next-images");
 
-const isProd = process.env.NODE_ENV === "production";
-console.log(isProd)
+// const isProd = process.env.NODE_ENV === "production";
+// console.log(isProd)
 
-module.exports = {
-  exportPathMap: () => ({
-    "/": { page: "/index" },
-  }),
-  assetPrefix: isProd ? "http://rutakeh.com/public_html/" : "",
-  publicRuntimeConfig: {
-    staticFolder: `/`,
-  },
-};
-module.exports = withImages();
-module.exports = withCSS(
-  withSass({
-    webpack(config, options) {
-      config.module.rules.push(
-        {
-          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|otf)$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 100000,
-              name: "[path][name].[ext]",
-            },
-          },
-        }
-      );
-      return config;
-    }
-  })
-);
+// module.exports = {
+//   exportPathMap: () => ({
+//     "/": { page: "/index" },
+//   }),
+//   assetPrefix: isProd ? "http://rutakeh.com/public_html/" : "",
+//   publicRuntimeConfig: {
+//     staticFolder: `/`,
+//   },
+// };
+// module.exports = withImages();
+// module.exports = withCSS(
+//   withSass({
+//     webpack(config, options) {
+//       config.module.rules.push(
+//         {
+//           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|otf)$/,
+//           use: {
+//             loader: "url-loader",
+//             options: {
+//               limit: 100000,
+//               name: "[path][name].[ext]",
+//             },
+//           },
+//         }
+//       );
+//       return config;
+//     }
+//   })
+// );
 
