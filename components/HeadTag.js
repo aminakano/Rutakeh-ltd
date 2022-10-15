9376cc96e5b079fa977ff1e9d7b940ec24d9608f
Ami Nakano <ami.nakano@outlook.com>
2022-10-15 14:30:25 +0200

    fixed script tag error

--- components/HeadTag.js
+++ components/HeadTag.js
@@ -1,20 +1,20 @@
 import Head from "next/head";
+import Script from "next/script";
 const HeadTag = () => (
-  <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-      name="keywords"
-      content="Rutakeh, 龍武株式会社, Rutakeh Limited, 龍武, リュウタケ, りゅうたけ, IT研修, プログラミング, 海外留学"/>
-    <meta
-      name="description"
-      content="IT業界向け各種トレーニング、新入社員教育、e-learning、海外留学、語学研修など、ご相談ください">
-    </meta>
-    <meta name="author" content="Rutakeh Limited" />
-    <script
-      type="text/javascript"
-      src="//webfonts.xserver.jp/js/xserver.js"
-    ></script>
-    <title>Rutakeh | 龍武株式会社</title>
-  </Head>
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="keywords"
+        content="Rutakeh, 龍武株式会社, Rutakeh Limited, 龍武, リュウタケ, りゅうたけ, IT研修, プログラミング, 海外留学"/>
+      <meta
+        name="description"
+        content="IT業界向け各種トレーニング、新入社員教育、e-learning、海外留学、語学研修など、ご相談ください">
+      </meta>
+      <meta name="author" content="Rutakeh Limited" />
+      <title>Rutakeh | 龍武株式会社</title>
+    </Head>
+    <Script src="//webfonts.xserver.jp/js/xserver.js"/>
+  </>
 );
 export default HeadTag;
